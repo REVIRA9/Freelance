1222e055c7113d4fcfe990a27af072616aad62fa
REVIRA9 <96121841+REVIRA9@users.noreply.github.com>
2025-06-07 17:34:35 +0700

    Add files via upload

--- TrackerApp/search.go
+++ TrackerApp/search.go
@@ -34,34 +34,12 @@ func CariProyek(DaftarProyek tabTrack, jumlahProyek int) {
 	}
 }
 
+
 func SeqSearch(DaftarProyek tabTrack, jumlahProyek int, namaYangDicari string) {
-	apakahKetemu := false
+	var	apakahKetemu bool = false
+
 	for i := 0; i < jumlahProyek && !apakahKetemu; i++ {
-		kataProyek := DaftarProyek[i].proyek
-		kataCari := namaYangDicari
-		kondisiCocok := false
-
-		if len(kataProyek) == len(kataCari) {
-			semuaHurufSama := true
-			for j := 0; j < len(kataProyek) && semuaHurufSama; j++ {
-				huruf1 := kataProyek[j]
-				huruf2 := kataCari[j]
-				if huruf1 >= 'A' && huruf1 <= 'Z' {
-					huruf1 += 32
-				}
-				if huruf2 >= 'A' && huruf2 <= 'Z' {
-					huruf2 += 32
-				}
-				if huruf1 != huruf2 {
-					semuaHurufSama = false
-				}
-			}
-			if semuaHurufSama {
-				kondisiCocok = true
-			}
-		}
-
-		if kondisiCocok {
+		if DaftarProyek[i].proyek == namaYangDicari {
 			tampilkanInfoProyekLengkap(DaftarProyek[i], i)
 			apakahKetemu = true
 		}
@@ -71,6 +49,7 @@ func SeqSearch(DaftarProyek tabTrack, jumlahProyek int, namaYangDicari string) {
 		fmt.Printf("Duh, maaf nih, proyek dengan nama '%s' kayaknya nggak ada di daftar.\n", namaYangDicari)
 	}
 }
+
 
 func BinarySearch(DaftarProyek tabTrack, jumlahProyek int, namaYangDicari string) {
 	indeksAwal := 0
